7586e8c4132eb4417ea57bb30e5a74ab802239c8
Nrdias <natandias.dev@gmail.com>
2023-11-22 20:13:48 -0300

    adicionado equipe

--- src/dados/Aplicacao.java
+++ src/dados/Aplicacao.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 
 public class Aplicacao {
     private ArrayList<Equipamento> equipamentos;
+    private ArrayList<Equipe> equipes;
 
     public Aplicacao() {
         this.equipamentos = new ArrayList<>();
+        this.equipes = new ArrayList<>();
     }
 
     public boolean addEquipamento(Equipamento e) {
@@ -17,6 +19,28 @@ public class Aplicacao {
         }
 
         return this.equipamentos.add(e);
+    }
+
+    public boolean addEquipe(Equipe e) {
+        boolean codinomeUsed = this.equipes.stream().anyMatch((equipe) -> (equipe.getCodinome().equals(e.getCodinome())));
+
+        if (codinomeUsed) {
+            return false;
+        }
+
+        this.equipes.add(e);
+        this.equipes.sort((o1, o2) -> o1.getCodinome().compareTo(o2.getCodinome()));
+        return true;
+    }
+
+    public String equipesToString(){
+        StringBuilder str = new StringBuilder("Equipes: \n");
+
+        this.equipes.forEach((equipe) -> {
+            str.append(equipe.toString()).append("\n");
+        });
+
+        return str.toString();
     }
 
     @Override
