791ae46ab479f5c9001112c0f537ae758d1da1d7
BrunoPanizzi <brunopanizziq@gmail.com>
2023-11-28 19:48:30 -0300

    finished alocar atendimento

--- src/telas/AlocarAtendimentos.java
+++ src/telas/AlocarAtendimentos.java
@@ -5,8 +5,12 @@ import dados.*;
 import javax.swing.*;
 import java.awt.*;
 
+// TODO: após alocar um atendimento, sugerir o próximo atendimento
 public class AlocarAtendimentos extends JPanel {
-    public Aplicacao app;
+    private Aplicacao app;
+    private Atendimento atendimento;
+    private JButton alocar;
+    private JTextArea messages;
 
     public AlocarAtendimentos(Aplicacao app) {
         super();
@@ -20,6 +24,7 @@ public class AlocarAtendimentos extends JPanel {
         centerPanel.setLayout(new GridBagLayout());
 
         GridBagConstraints gc = new GridBagConstraints();
+        gc.fill = GridBagConstraints.BOTH;
         gc.gridx = 0;
         gc.gridy = 0;
 
@@ -29,19 +34,26 @@ public class AlocarAtendimentos extends JPanel {
             centerPanel.add(new JLabel("Não há atendimentos pendentes! :)"), gc);
         } else {
             centerPanel.add(new JLabel("Quantidade de atendimentos pendentes: " + pendentes), gc);
+
+            this.atendimento = this.app.peekAtendimento();
+
             gc.gridy++;
-            JPanel proximoAtendimento = this.renderAtendimento(this.app.peekAtendimento());
-            centerPanel.add(proximoAtendimento, gc);
+            centerPanel.add(this.renderAtendimento(), gc);
             gc.gridy++;
-            JButton alocar = new JButton("Alocar");
+            alocar = new JButton("Alocar");
+            alocar.addActionListener(e -> this.handleAlocar());
             centerPanel.add(alocar, gc);
+            gc.gridy++;
+            messages = new JTextArea(5, 20);
+            messages.setEditable(false);
+            centerPanel.add(messages, gc);
         }
 
         this.add(centerPanel);
 
     }
 
-    private JPanel renderAtendimento(Atendimento atendimento) {
+    private JPanel renderAtendimento() {
         JPanel atendimentoPanel = new JPanel();
         atendimentoPanel.setLayout(new GridLayout(3, 2));
 
@@ -65,5 +77,18 @@ public class AlocarAtendimentos extends JPanel {
         atendimentoPanel.setBackground(Color.LIGHT_GRAY);
 
         return atendimentoPanel;
+    }
+
+    private void handleAlocar() {
+        String message = this.app.alocarAtendimento();
+
+        if (message == null) {
+            this.messages.setText("Atendimento alocado com sucesso!\n");
+            this.messages.append("Equipe designada: " + atendimento.getEquipe());
+            this.alocar.setEnabled(false);
+        } else {
+            this.messages.setText("Não foi possível alocar o atendimento.\nMotivo: " + message + ".");
+            this.alocar.setEnabled(false);
+        }
     }
 }
